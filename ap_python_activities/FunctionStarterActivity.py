436b1b1b4d7a89b727ad245777d0abbf00e4e637
newgod215 <130468174+newgod215@users.noreply.github.com>
2023-09-25 03:37:48 +0000

    work

---
+++ ap_python_activities/FunctionStarterActivity.py
@@ -0,0 +1,25 @@
+# 1. In your own words, describe what a function is
+ a relation between a set of inputs having one output each.
+
+# 2. What is are function parameters and arguments and describe
+# the difference between the 2.
+A parameter is a variable in a function definition. It is a placeholder and hence does not have a concrete value.
+
+# 3. Write a function that will print out a welcome message
+# that includes a user's name. You will need to use parameters and arguments
+greeting{makai}
+print{ welcome to python class}
+
+# 4. Write a function that will do a calculation that takes 3 parameters.
+# Your function can do any of the arithmetic operators (add, subtract, multiply, divide)
+number_of_pencils:100
+number_of_pens:213
+print{numberA + numberB}
+print{100+213}
+# 5. Write a function that will output a message to a user, telling them
+# what class they have next after this one. this code should use a
+# variable to pass a value into the parameter of a function. The variable should
+# be real, user data- not hard-coded data.
+print greeting{makai}
+print {welcome_to_coding_class}
+print{next_class_math}
