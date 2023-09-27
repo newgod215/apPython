d9fe2005325178e2db9624d0da5b75be30457ca3
newgod215 <130468174+newgod215@users.noreply.github.com>
2023-09-27 15:23:17 +0000

    didnt get to do the last one in time

---
+++ ap_python_activities/conditionalActivity.py
@@ -0,0 +1,31 @@
+# 1. What is the difference between 
+# a parameter and an argument in a python function
+
+A parameter is the variable listed inside the parentheses in the function definition. An argument is the value that are sent to the function when it is called.
+
+# 2. In your own words, describe what 
+# a conditional statement (if/else) is 
+If the condition inside the parentheses evaluates to true , the code inside the if block will execute.
+
+# 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
+if (age < 18) text = "old enough to buy alcohol
+
+# 3. Write a conditional statement for food in the refridgerator.
+# your conditional statement should be wrapped in a function that takes one (1)
+# parameter. The data type for this parameter should be true or false. 
+# your function should have a line of code that asks the user if there is food in the refridgerator.
+# If there is food in the refridgerator, print time to cook. If there is 
+# NOT food in the fridge, print time to shop. 
+# When you call your function there should be an argument that accepts 
+# a boolean. 
+
+
+ Is there is food in the refridgeraor
+food = true    
+no food = False
+
+# 4. Create a function that checks the inventory of cereal for a store. 
+# your function should parameter should accept an integer. In your function
+# use a conditional statement to determine if you need to order more cereal.
+# If there is more than 10 boxes, print "inventory full", else if there are less than 
+# 10 boxes print "we need to order more cereal".
