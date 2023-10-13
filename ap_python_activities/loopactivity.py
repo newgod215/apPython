42d9cdd08e319e5986b624b6cd066601cae2983b
newgod215 <130468174+newgod215@users.noreply.github.com>
2023-10-13 15:21:01 +0000

    only 1-4

---
+++ ap_python_activities/loopactivity.py
@@ -0,0 +1,54 @@
+# 1. Provided below is some starter code. 
+# Fix this code to create a loop that will iterate
+# until it gets to the number 10.
+
+# hint: remember there are 3 parts to a loop. 
+i=0
+while i = 0.
+    print('i < 10')
+    print(i)
+    if i =1
+    
+
+# 2. Create a simple while loop that will iterate over a the following condition:
+# The loop will ask the user to enter the secret number. The secret number is 3. 
+# If the user enters the secret number correctly, the loop will stop and tell the user
+# the can win a prize. 
+# If the user puts in an incorrect secret number, the loop will ask them to enter the 
+# correct secret number. 
+if the_secret_number = 3
+ print(3 < prize)
+else  the secret number isnt 3
+print(2 < no prize)
+
+# 3. describe the difference between a while loop and a conditional statement. 
+# Try be as specific as you can
+A while loop will run as many times as it needs to while a condition is true, i.e., 
+until that condition is false. An if statement will execute once if a condition is true.
+
+# 4. Create a while loop that will iterate over the list of names 
+# and print out only the following name: William
+
+
+list of names ['Avery','Paige','William','Ade','Jasmyn','Crystal']
+
+index = 1
+names = ['Avery', 'William', 'ade' , 'Paige' , 'Jasmyn' , 'Crystal']
+
+for i in names: 
+  print(index, '.' ,' ', i, sep='')
+  index += 1
+
+#OUTPUT:
+#1. Avery
+#2. William
+#3. ade
+#4. Paige
+#5.Jasmyn
+#6.Crystal
+
+# 5. Create a  loop that will ask the user to add a new string value to the list of names above.
+# hint you will need use append().
+
+# BONUS QUESTION
+# Describe the differences between a FOR loop and WHILE loop. 
