28bda3502118a44dde4f9736470ca5cd1565db09
newgod215 <130468174+newgod215@users.noreply.github.com>
2023-10-19 02:15:35 +0000

    done I guess

---
+++ ap_python_activities/whileLoops.py
@@ -0,0 +1,13 @@
+#while loops 
+camravision = 0 
+while camrasenor == true
+print["no one in sight"]
+peopleinsight = input{is there anyone in sight? enter 1 if yes 2 if no }
+if sightsensor == yes
+break
+
+
+printer
+while int{input{"12 papers"} 
+    printer>=0
+print{"print papers"}
