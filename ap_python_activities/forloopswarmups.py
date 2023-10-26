2c962e6ee66837b26680be27a58cf626957534c0
newgod215 <130468174+newgod215@users.noreply.github.com>
2023-10-26 14:54:39 +0000

    did my best

---
+++ ap_python_activities/forloopswarmups.py
@@ -0,0 +1,12 @@
+# 1. In your own words, describe what a for loop is?
+used for iterating over a sequence (that is either a list, a dictionary, a set, or a string).
+# 2. What is the difference between a FOR loop and a WHILE loop?
+# Provide two (2) examples of each. 
+while loop: 1-execute a set of statements as long as a condition is true.
+2-requires relevant variables to be ready
+for loops: 1 - just like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
+2-for loop can execute a set of statements, once for each item in a list, tuple, set etc.
+# 3. Create a FOR loop that will go through a list of names 
+# and print all the names that start with the letter "R".
+
+names=['Michael','Rebecca','William','Kareem','Robert','Rose','Jason']
