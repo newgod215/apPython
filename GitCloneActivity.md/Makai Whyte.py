a94704b1d5bd5cb215f533c30b986e453654ec57
newgod215 <130468174+newgod215@users.noreply.github.com>
2023-11-03 15:20:28 +0000

    All done

---
+++ GitCloneActivity.md/Makai Whyte.py
@@ -0,0 +1,18 @@
+ListOfkids =["'micheal',john,'gwen',jordon,'jaseil,'jennifer'"]
+
+{"who is in class and who isnt in clas"}
+if{ all the kids are here say "here"}
+
+else { the kids isnt here they stay "silnce"}
+while print{listOfname}
+print{ if all the kids are here say "here"}
+print{if kids are not here they "stay slinced" }
+
+
+1. Was it easy to find your classmates assignment? Yes
+2. Was their documentation clear and concise on what their program was designed to do? also yes
+3. Was their documnetion clear and concise on how to install their program? again also yes
+4. When you installed and ran your classmates program, did it work as described? It was pretty good when I ran it
+5. Based on your experience using your classmate's program, as well as what you've learned about programming and documentation,
+are there any reccomendations that you would provide to your classmate? #Makai has a very different way of thing about coding than me so I look up to the 
+#way he thinks when it comes to this class so I dont have much to recomend to him.
