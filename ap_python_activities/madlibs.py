c861954f2bb31b9db88225359756193e299ba248
newgod215 <130468174+newgod215@users.noreply.github.com>
2024-01-10 16:08:54 +0000

    madlib completed

---
+++ ap_python_activities/madlibs.py
@@ -0,0 +1,26 @@
+#  Winter break Python Mad libs 
+# Create a short mad lib story about your winter break. 
+# Your mad lib should include 6 input fields for users to enter data.
+# Your mad lib program should prompt the user to enter 2 nouns, 2 verbs, and 2 adjectives. 
+# Your mad lib should be written in complete sentences. 
+# Once completed, submit your madlib to your github repository by using the source control tool in codespaces. 
+
+
+def story():
+    # madlibs_story 
+    adjective = input("mostly wintry ")
+    adjective2 = input("zero snow ") 
+    noun = input(" a fast winter break ")
+    noun2 = input(" christmas short but fun")
+    plural_noun = input("gifts for many of use ")
+    plural_noun2 = input("many songs to hear ")
+    ing_verb = input("lots of singing from all")
+    ing_verb2 = input("lots of playing from kids ")
+    plural_noun3 = input("different places to go ")
+    ing_verb3 = input("opening gifts with all ")
+    place = input("grandma's house was a blast ")
+    ing_verb4 = input("having fun with everyone")
+    adjective3 = input("entertaining for all ")
+    plural_noun4 = input(" with alot of amusement ") 
+
+print(story()) 
